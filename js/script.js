04e72626090ce1bd60bfb5014b311de9d0c9678d
dany339 <hyd55080001@gmail.com>
2025-04-25 18:04:44 +0900

    브랜드소개 css 작업중

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-const $navIcon = $(".nav-icon");
-const $navBar = $(".navbar");
-
-$navIcon.on("click", function () {
-  $navBar.toggleClass("active");
-  $navIcon.toggleClass("active");
+/* MOBILE MENU */
+$("header .nav .nav-icon").click(function () {
+  $(this).toggleClass("active");
+  $("header .navbar").toggleClass("active");
 });
 
+/* TABS FUNCTION */
+
+$(".tabs li").click(function () {
+  $(".tab-content").removeClass("active");
+  $(".tabs li").removeClass("active");
+  $(this).addClass("active");
+
+  var tabId = $(this).data("tab");
+  $("#" + tabId).addClass("active");
+
+  $(".tabs-wrap").removeClass("active");
+});
+
+/* MAIN */
 if ($(".swiper-container")) {
   const sec1Swiper = new Swiper(".swiper-container", {
     slidesPerView: 1,
@@ -30,6 +42,21 @@ if ($(".swiper-container2")) {
     },
     pagination: {
       el: ".swiper-container2 .swiper-pagination2",
+      clickable: true,
+    },
+  });
+}
+
+/* HISTORY */
+if ($(".slick-list")) {
+  const slickSwiper = new Swiper(".slick-list", {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    pagination: {
+      el: ".slick-list .swiper-pagination2",
       clickable: true,
     },
   });
